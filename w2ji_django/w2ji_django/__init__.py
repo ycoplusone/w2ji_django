09ae09022e05028ce10137fbead2bd86a97b6b59
DLIVE <DLIVE@DESKTOP-6OUHHUK>
2020-12-16 16:41:19 +0900

    20201216-1641

---
+++ w2ji_django/w2ji_django/__init__.py
@@ -0,0 +1,9 @@
+'''
+장고 쉘에서 테스트하기 : shell with django Environment 실행
+manage.py shell 
+from w2ji_bbs.models import bbs
+a = bbs.objects.create(title='aaaa' , content='content 1111' , author='정몰?' , create_dt = '2020-12-16'  )
+a.author
+
+되겠지?
+'''

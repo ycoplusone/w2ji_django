820803d96d3e09d27feb0605b52bd58c27d41cc7
User <User@DESKTOP-N15QPAP>
2021-01-05 14:22:07 +0900

    20210105-1422

--- w2ji_django/w2ji_django/settings.py
+++ w2ji_django/w2ji_django/settings.py
@@ -137,18 +137,14 @@ LANGUAGE_CODE = 'ko-kr'
 TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True #기본시간대(UTC)에 대해서 사용 유무를 선택함.
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR , 'static')]
-
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR , 'media')

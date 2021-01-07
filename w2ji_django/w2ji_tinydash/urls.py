d77970f0ca1c679fb830fdfd0efbbb79b38bce66
User <User@DESKTOP-N15QPAP>
2021-01-07 17:57:49 +0900

    20210107-1757

--- w2ji_django/w2ji_tinydash/urls.py
+++ w2ji_django/w2ji_tinydash/urls.py
@@ -3,9 +3,13 @@ from . import views
 
 app_name = 'w2ji_tinydash'
 urlpatterns = [    
-    
-    #path('hello/<to>'       , views.hello ) ,
-    path(''             , views.index.as_view()  ) ,
-    
-    
+    path(''                             , views.index.as_view()     ) ,
+    path('ui-color/'                    , views.uicolor.as_view()   ) ,    
+    path('ui-typograpy/'                , views.uitypograpy.as_view()   ) ,
+    path('ui-icons/'                    , views.uiicons.as_view()   ) ,
+    path('ui-buttons/'                  , views.uibuttons.as_view() ) ,
+    path('ui-notification/'             , views.uinotification.as_view()   ) ,
+    path('ui-modals/'                   , views.uimodals.as_view()   ) ,
+    path('ui-tabs-accordion/'           , views.uitabsaccordion.as_view()   ) ,
+    path('ui-progress/'                 , views.uiprogress.as_view()   ) ,
 ]
--- w2ji_django/w2ji_tinydash/views.py
+++ w2ji_django/w2ji_tinydash/views.py
@@ -12,3 +12,28 @@ class index(TemplateView):  #목록보기
             #'lists' : self.get_queryset() ,    #self.queryset #걸색결과 
         }    #템플릿에 전달할 데이터
         return self.render_to_response(ctx)
+
+class uicolor(TemplateView):
+    template_name = 'ui-color.html' #뷰 전용 템플릿 생성
+
+class uitypograpy(TemplateView):
+    template_name = 'ui-typograpy.html' #뷰 전용 템플릿 생성
+
+class uiicons(TemplateView):
+    template_name = 'ui-icons.html' #뷰 전용 템플릿 생성
+
+class uibuttons(TemplateView):
+    template_name = 'ui-buttons.html' #뷰 전용 템플릿 생성
+
+class uinotification(TemplateView):
+    template_name = 'ui-notification.html' #뷰 전용 템플릿 생성
+
+class uimodals(TemplateView):
+    template_name = 'ui-modals.html' #뷰 전용 템플릿 생성
+
+class uitabsaccordion(TemplateView):
+    template_name = 'ui-tabs-accordion.html' #뷰 전용 템플릿 생성
+
+class uiprogress(TemplateView):
+    template_name = 'ui-progress.html' #뷰 전용 템플릿 생성
+        
